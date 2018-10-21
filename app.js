f8b567906a16dda56ddfb8bdb00b5c785a692a01
ajiagahari <pembikinjanda@gmail.com>
2018-10-21 22:47:15 +0700

    master : handle click inside iframe

--- app.js
+++ app.js
@@ -6,6 +6,19 @@ require('es6-promise').polyfill();
 //require('isomorphic-fetch')
 var fetchUrl = require("fetch").fetchUrl;
 
+function myMiddleware (req, res, next) {
+   if (req.method === 'GET') {
+     // Do some code
+     console.log(req.url)
+   }
+   // keep executing the router middleware
+   next()
+}
+
+app.use(myMiddleware)
+
+
+
 app.get('/website', (req, res) => {
 	res.sendFile(path.join(__dirname+'/index.html'));
 })
@@ -15,4 +28,8 @@ app.get('/*',(req,res) => {
 		res.set('Content-Type', 'text/html').status(200).send(body);
 	})
 })
+
+
+
+
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
